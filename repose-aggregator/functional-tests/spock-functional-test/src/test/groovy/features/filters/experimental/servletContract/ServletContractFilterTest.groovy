340b8a2384ce938fba09998dffdb7e4391b2157e
David Kowis <david.kowis@rackspace.com>
2014-05-07 13:58:00 -0500

    Setting up splosiontime

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/experimental/servletContract/ServletContractFilterTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/experimental/servletContract/ServletContractFilterTest.groovy
@@ -1,33 +1,37 @@
 package features.filters.experimental.servletContract
 
 import framework.ReposeValveTest
+import org.junit.Assume
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.MessageChain
 
 class ServletContractFilterTest extends ReposeValveTest {
 
-    def setupSpec() {
+    def splodeDate = new Date(2014 - 1900, Calendar.JULY, 1, 9, 0);
+
+    /**
+     * This test fails because repose does not properly support the servlet filter contract.
+     * It should not fail.
+     *
+     * This test is ignored until JULY of 2014. The same splosion date as other ones. It should probably be ignored
+     * until further than that, but I'm not sure what to do about that there.
+     * @return
+     */
+    def "Proving that a custom filter does in fact work" () {
+        setup:
+        Assume.assumeTrue(new Date() > splodeDate)
+
         def params = properties.defaultTemplateParams
         repose.configurationProvider.applyConfigs("common", params)
         repose.configurationProvider.applyConfigs("features/filters/experimental/servletfilter", params)
 
         deproxy = new Deproxy()
         deproxy.addEndpoint(properties.targetPort)
-
+        def started = true
         repose.start([waitOnJmxAfterStarting: false])
+
         waitUntilReadyToServiceRequests("200")
-    }
 
-    def cleanupSpec() {
-        repose.stop()
-        deproxy.shutdown()
-    }
-
-    /**
-     * This test fails because repose does not properly support the servlet filter contract
-     * @return
-     */
-    def "Proving that a custom filter does in fact work" () {
         when:
         MessageChain mc = null
         mc = deproxy.makeRequest(
@@ -40,5 +44,12 @@ class ServletContractFilterTest extends ReposeValveTest {
         then:
         mc.receivedResponse.code == '200'
         mc.receivedResponse.body.contains("<extra> Added by TestFilter, should also see the rest of the content </extra>")
+
+        cleanup:
+        if(started)
+            repose.stop()
+        if(deproxy != null)
+            deproxy.shutdown()
+
     }
 }
